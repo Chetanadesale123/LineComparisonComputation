a96dec2d2b6844197bb56e072964fe77863924eb
59114_Chetana <deslechetna1999@gmail.com>
2022-04-08 01:12:48 +0530

    [chetana] Added : Checking the equality between two line

--- LineComparisonComputation/Program.cs
+++ LineComparisonComputation/Program.cs
@@ -6,11 +6,19 @@ namespace LineComparisonComputation
     {
         static void Main(string[] args)
         {
-            LineComparison computation= new LineComparison(35,74,82,100);
-             computation.CalculateLength();
-            
+            LineComparison computation = new LineComparison(35, 74, 82, 100);
+            double line1 = computation.CalculateLength();
+            LineComparison line = new LineComparison(30, 64, 89, 118);
+            double line2 = line.CalculateLength();
+            if (line1.Equals(line2))
+                Console.WriteLine("Both lines lengths are equal");
+            else
+                Console.WriteLine(" Both lines are not equal");
+
+
+
         }
-        
+
 
 
     }
